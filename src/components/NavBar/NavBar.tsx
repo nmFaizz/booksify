99287e23d23131c6f39eb305097b778d9072221e
nmFaizz <ilznorz582@gmail.com>
2024-01-11 23:45:20 +0700

    new navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,10 +3,25 @@ import Image from "next/image";
 import NavLinks from "./NavLinks/NavLinks";
 import SearchBar from "../SearchBar/SearchBar";
 import { TbMenu } from "react-icons/tb";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function NavBar() {
     const [isMenu, setIsMenu] = useState(false)
+    const [isNav, setIsNav] = useState(false)
+
+    const scrollHandler = () => {
+        if (window.scrollY >= 30) {
+            setIsNav(() => true)
+        } else {
+            setIsNav(() => false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', scrollHandler)
+
+        return () => window.removeEventListener('scroll', scrollHandler)
+    }, [])
 
     const navOption = [
         {
@@ -30,56 +45,56 @@ export default function NavBar() {
     const isMenuHandler = (): void => {
         setIsMenu(() => false)
     }
-
-    const navScrollHandler = (e: any) => {
-        console.log(e)
-    }
+    
 
     return (
         <React.StrictMode>
-            <header className="w-full  px-4 sm:px-12 flex justify-between items-center py-2 sticky top-0 z-50 text-stone-700" onClick={navScrollHandler}>
-                <div>
-                    <p className="text-2xl">BOOKSIFY</p>
+            <header className="sticky top-0 z-50">
+                <div className={`w-full ${isNav ? 'bg-white' : 'bg-none text-stone-700'} px-4 sm:px-12 flex justify-between items-center py-2`}>
+                    <div>
+                        <p className="text-2xl">BOOKSIFY</p>
+                    </div>
+                    
+                    <nav className="flex gap-4 sm:gap-12 items-center">
+                        <div className="gap-12 lg:flex hidden">
+                            {navOption.map(option => 
+                                <NavLinks key={option.name} path={option.pathName}  linkName={option.name} isMenuHandler={isMenuHandler}/>
+                            )}
+                        </div>
+
+                        <div className="hidden lg:block">
+                            <SearchBar />
+
+                        </div>
+
+
+                        <div className="flex lg:hidden items-center" onClick={() => setIsMenu((prev: boolean) => !prev)}>
+                            <TbMenu className="text-2xl" />
+                        </div>
+
+                        <div className="w-[40px] h-[40px] rounded-full bg-stone-700 overflow-hidden">
+                            <Image 
+                                src='/guest-pfp.jpg' 
+                                alt="guest-pfp" 
+                                width={0} height={0} 
+                                style={{objectFit: 'cover', width: '100%', height: '100%'}} 
+                            />
+                        </div>
+                    </nav>
                 </div>
-                
-                <nav className="flex gap-4 sm:gap-12 items-center">
-                    <div className="gap-12 lg:flex hidden">
-                        {navOption.map(option => 
-                            <NavLinks key={option.name} path={option.pathName}  linkName={option.name} isMenuHandler={isMenuHandler}/>
-                        )}
-                    </div>
 
-                    <div className="hidden lg:block">
+                <nav className={`px-4 z-[-1] gap-12 lg:hidden ${isNav ? 'bg-white' : 'bg-stone-100'} flex fixed w-full flex-col py-12 text-stone-700 ${isMenu ? 'translate-y-[0%]' : 'translate-y-[-150%]'} transition-all text-stone-700`}>
+                    <div className="">
                         <SearchBar />
-
                     </div>
 
+                    {navOption.map(option => 
+                        <NavLinks key={option.name} path={option.pathName}  linkName={option.name} isMenuHandler={isMenuHandler}/>
+                    )}
 
-                    <div className="flex lg:hidden items-center" onClick={() => setIsMenu((prev: boolean) => !prev)}>
-                        <TbMenu className="text-2xl" />
-                    </div>
-
-                    <div className="w-[40px] h-[40px] rounded-full bg-stone-700 overflow-hidden">
-                        <Image 
-                            src='/guest-pfp.jpg' 
-                            alt="guest-pfp" 
-                            width={0} height={0} 
-                            style={{objectFit: 'cover', width: '100%', height: '100%'}} 
-                        />
-                    </div>
                 </nav>
             </header>
-
-            <nav className={`px-4 gap-12 lg:hidden flex fixed w-full flex-col py-12 text-white ${isMenu ? 'translate-y-[0%]' : 'translate-y-[-150%]'} transition-all text-stone-700`}>
-                <div className="">
-                    <SearchBar />
-                </div>
-
-                {navOption.map(option => 
-                    <NavLinks key={option.name} path={option.pathName}  linkName={option.name} isMenuHandler={isMenuHandler}/>
-                )}
-
-            </nav>
+            
         </React.StrictMode>
     ) 
 }
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -7,9 +7,9 @@ const SearchBar = () => {
 
 
     return (
-        <div className='bg-stone-700 py-2 px-2 items-center rounded-md flex'>
-            <input type="text" className='bg-transparent text-sm focus:outline-none lg:w-[230px] w-full text-white' placeholder="Search books.."/>
-            <IoIosSearch className="text-lg text-white" onClick={KeyHandler} />
+        <div className='bg-none border-[1px] border-black py-2 px-2 items-center rounded-sm flex text-stone-700'>
+            <input type="text" className='bg-transparent text-sm focus:outline-none lg:w-[230px] w-full' placeholder="Search books.."/>
+            <IoIosSearch className="text-lg" onClick={KeyHandler} />
         </div>
     )
 }
